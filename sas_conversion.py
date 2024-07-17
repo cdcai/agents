88aec4062658f4e8576dd8a2623f18a0b9627f0d
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2024-07-17 16:36:26 -0400

    Adding argparse, logging, etc.

--- sas_conversion.py
+++ sas_conversion.py
@@ -1,20 +1,24 @@
 """
 Testing Conversion of SAS files to Python using system of language agents
 """
+from argparse import ArgumentParser
+
 import os
 import re
-from pathlib import Path
 from typing import Union
 from openai import OpenAI
 import tiktoken
-
+import logging
 from dotenv import load_dotenv
 
 from util import Agent
 
 load_dotenv()
 
-PYSCRIPT_CONTENT = re.compile(r"```py(.+?)```", re.DOTALL)
+PYSCRIPT_CONTENT = re.compile(r"```python(.+?)```", re.DOTALL)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 class SASConvertAgent(Agent):
     """
@@ -115,12 +119,26 @@ class SASConvertAgent(Agent):
         super().reset()
 
 if __name__ == "__main__":
-    sas_file = Path("data") / "INPUT - GEN LAB 1 initial LOINC pull and data clean (5).sas"
+    parser = ArgumentParser(
+        prog="SAS -> Python Conversion via LLM agents",
+        description="""
+        This program converts an input SAS file into a rough outline of a Python script that might replace the functionality.
+        It's unlikely that the resulting script will work out-of-the-box, but it should provide a basic structure to start with
+        """
+    )
+    parser.add_argument("-i", "--input", type=str, default="data/INPUT - GEN LAB 1 initial LOINC pull and data clean (5).sas", help="SAS file input path")
+    parser.add_argument("-o", "--output", type=str, default="out.py", help="Python file output path (default: out.py)")
+    parser.add_argument("-m", "--model", type=str, default="edav-chatapp-share-gpt4-32k-tpm25kplus-v0613-dfilter", help="Name of model deployment to use for language agents (default: edav-chatapp-share-gpt4-32k-tpm25kplus-v0613-dfilter)")
+    parser.add_argument("--device_code", action="store_true", help="Instead of using Service Principal creds, utilize Azure Device Code authorization flow for Azure OpenAI")
+    args = parser.parse_args()
 
-    with open(sas_file, "r") as file:
+    with open(args.input, "r") as file:
         sas_file_content = file.read().strip()
     
-    sas_agent = SASConvertAgent(sas_file_content, model_name="edav-api-share-gpt4-api-nofilter")
+    if args.device_code:
+        # TODO: Implement
+        pass
+    sas_agent = SASConvertAgent(sas_file_content, model_name=args.model)
 
     sas_agent.run()
 
