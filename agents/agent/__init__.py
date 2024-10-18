c441db62001f5dd4b8a32d5272ef9f2bc2583449
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2024-10-18 16:14:22 -0400

    whoops

--- agents/agent/__init__.py
+++ agents/agent/__init__.py
@@ -3,4 +3,8 @@ from .sas import *
 from .reflexion import *
 from .prediction import *
 
-__all__ = ["ReactAgent", "ReactandReflectAgent", "SASConvertAgent", "PythonRefineAgent", "CodeOutlinerAgent", "OutlineSummarizeAgent", "PythonSummarizeAgent", "PseudocodeSummarizeAgent", "PredictionAgent"]
+__all__ = [
+    "ReactAgent", "ReactandReflectAgent", "SASConvertAgent", "PythonRefineAgent",
+    "CodeOutlinerAgent", "OutlineSummarizeAgent", "PythonSummarizeAgent", "PseudocodeSummarizeAgent",
+    "PredictionAgent", "PredictionAgentWithJustification"
+]
