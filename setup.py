d27b71488289f94d6ed75d8cc227167d0c97a7ad
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2025-01-03 10:58:56 -0500

    Update setup.py
    Bumping to v0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", encoding="utf-8") as readme_file:
 
 setup(
     name='agents',
-    version='0.3.0-alpha.1',
+    version='0.3.0',
     description='Yet another langchain-esque package to use language agents and compose agent-systems',
     long_description=readme,
     long_description_content_type="text/markdown",
