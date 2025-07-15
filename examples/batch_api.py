d118b4796f457f5f68304980f9ee47e29ce3e601
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2025-07-15 17:25:58 -0400

    Adding OpenAI Batch API support (#23)
    # Changes
    
    ## Misc
    - `tests/` -> `test/`
    - Asyncio version of tqdm used instead of standard variant (probably not needed)
    
    ## Adding OpenAI Batch API support
     
    I added a new `AzureOpenAIBatchProvider` provider class which works in conjunction with a new `_BatchAPIHelper` class to gather all OpenAI web requests into a single batch request, process that request, and return the results back to the requesting agents 
     
    This should involve some *breaking* changes to the API, notably:
    - Providers now call `endpoint_fn`, which is monkey-patched depending on whether chat or batch endpoints are required.
    - Providers get async with entry/exit points for async task cleanup in the batch case (superfluous in the chat endpoint case)
    - Substantial re-writing of the Processor class (see below)
    
    ### Example Usage
    https://github.com/cdcai/agents/blob/ba5f99dc472dd45f9d0375414ccba638f64d9f04/examples/batch_api.py#L108-L116
    
    ## Processor Changes
    
    Overhauled the standard Processor logic to handle batch API usage.
    
    ### Rationale
    
    Previous method fired only as many agents as there were requested workers at any given time. This is inefficient for the Batch API case where we'd like to fire all the agents at once and let the batcher handle processing them all.
    
    ### API Changes
    
    - `ProcessorIterable` / `ProcessorDF` are the chat endpoint variants
    - `BatchProcessorIterable` / `BatchProcessorDF` are the batch endpoint variants
    - Provider is now a required field upon init to handle case batch case

---
+++ examples/batch_api.py
@@ -0,0 +1,146 @@
+"""
+Testing the OpenAI Batch API in a few different scenarios.
+"""
+
+import asyncio
+import logging
+from typing import List, Optional
+
+import pydantic
+from dotenv import load_dotenv
+
+import agents
+
+load_dotenv()
+
+logging.basicConfig(filename="batch_api.log", filemode="w", level=logging.INFO)
+
+# A knock-knock joke return model
+class KnockKnock(pydantic.BaseModel):
+    setup: str = pydantic.Field(description="The setup for the knock-knock joke")
+    punchline: str = pydantic.Field(
+        description="The punchline for the knock-knock joke"
+    )
+
+    def __str__(self) -> str:
+        return f"- Knock-Knock.\n- Who's There?\n- {self.setup}\n- {self.setup} who?\n- {self.punchline}"
+
+    def __repr__(self) -> str:
+        return f"KnockKnock(setup={self.setup!r}, punchline={self.punchline!r})"
+
+# Define an agent
+class KnockKnockAgent(agents.StructuredOutputAgent):
+    "An agent that writes good Knock Knock jokes."
+
+    BASE_PROMPT = """
+    You are a top-comedian that specializes in writing knock-knock jokes.
+
+    You're in a competition to write the best knock-knock joke, and you have to really think of a unique joke to win.
+
+    You'll have two tasks:
+    1. Plan for how you'd write a knock-knock joke that would win in a competition using the plan() tool
+    2. Write the joke using the KnockKnock model, which should consist of a setup (the part that follows "who's there?") and a punchline (the part that follows "who?").
+    """
+
+    # Making temp higher
+    oai_kwargs = {"temperature": 0.9, "parallel_tool_calls": False}
+
+    def __init__(
+        self,
+        model_name: Optional[str] = None,
+        stopping_condition=None,
+        provider=None,
+        tools=None,
+        callbacks=None,
+        oai_kwargs=None,
+        **fmt_kwargs,
+    ):
+
+        super().__init__(
+            response_model=KnockKnock,
+            model_name=model_name,
+            stopping_condition=stopping_condition,
+            provider=provider,
+            tools=tools,
+            callback=callbacks,
+            oai_kwargs=oai_kwargs,
+            **fmt_kwargs,
+        )
+
+    @agents.agent_callable(
+        "Plan for how you'd write a knock-knock joke that would win in a competition.",
+        {"text": "Your plan for the joke."},
+    )
+    def plan(self, text: str) -> str:
+        return "Good thinking. Now send your joke."
+
+class KnockKnockJudge(agents.PredictionAgent):
+    """
+    The Knock-knock joke contest Judge
+    """
+
+    BASE_PROMPT = """
+    You are the judge of a knock-knock joke competition.
+    You know a good joke when you hear it, and it's time to crown a winner.
+
+    Read the following knock-knock jokes and crown a winner using a tool call with the joke number:
+
+    {jokes}
+    """.strip()
+
+    def __init__(self, jokes: List[str], provider=None, **fmt_kwargs):
+        labels = [str(i) for i, _ in enumerate(jokes)]
+
+        if fmt_kwargs is None:
+            fmt_kwargs = {}
+
+        fmt_kwargs.update(
+            {
+                "jokes": "\n".join(
+                    f"Joke {i}:\n{joke}" for i, joke in enumerate(jokes)
+                )
+            }
+        )
+
+        super().__init__(labels=labels, provider=provider, **fmt_kwargs)
+
+async def agents_example():
+    async with agents.AzureOpenAIBatchProvider(
+        "gpt-4o-batch", batch_size=5, n_workers=2
+    ) as provider:
+        # Kind of a hacky way to use this, but just for demonstration purposes
+        proc = agents.BatchProcessorIterable(
+            [i for i in range(10)], KnockKnockAgent, batch_size=1, provider=provider
+        )
+
+        jokes = await proc.process()
+
+    print("Got the following entries:")
+    print(
+        "\n".join(
+            f"Joke {i}:\n{str(KnockKnock.model_validate(res))}"
+            for i, res in enumerate(jokes)
+        )
+    )
+
+    # Judge the jokes
+    judge = KnockKnockJudge(
+        [str(KnockKnock.model_validate(joke)) for joke in jokes],
+        # Chat provider since it's a single call
+        provider=agents.AzureOpenAIProvider(
+            model_name="gpt-4o-nofilter", interactive=False
+        ),
+    )
+
+    await judge()
+    best_joke_idx = judge.answer["labels"][0]
+
+    print(
+        "The judge crowned a winner!\n\n{}".format(
+            str(KnockKnock.model_validate(jokes[int(best_joke_idx)]))
+        )
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(agents_example())
---
+++ test/test_batch_api.py
@@ -0,0 +1,200 @@
+"""
+Test OpenAI batch API functionality
+"""
+import asyncio
+import json
+import time
+from copy import copy
+
+import openai
+import pytest
+from openai._legacy_response import HttpxBinaryResponseContent
+from openai.resources.batches import AsyncBatches
+from openai.resources.files import AsyncFiles
+from openai.types import Batch, FileObject
+from pytest_mock import MockFixture
+
+import agents
+
+# Mock returns
+mock_batch_file_ret = ("abc.txt", b"lorem ipsum", "application/json")
+
+mock_file_obj = FileObject(
+    id="file_123",
+    bytes=1024,
+    created_at=0,
+    filename=mock_batch_file_ret[0],
+    object="file",
+    purpose="batch",
+    status="uploaded",
+)
+
+mock_batch = Batch(
+    id="batch_123",
+    completion_window="24h",
+    created_at=0,
+    endpoint="/v1/chat/completions",
+    input_file_id=mock_file_obj.id,
+    output_file_id=mock_file_obj.id,
+    object="batch",
+    status="validating",
+)
+
+
+# Simulate a fake batch response body
+def gen_response_body(n: int) -> bytes:
+    """
+    Generates a mock response body for batch completions.
+    """
+    response_body = [
+        {
+            "id": f"batch_{i + 1}",
+            "custom_id": f"task-{i + 1}",
+            "response": {
+                "status_code": 200,
+                "request_id": "req_1234",
+                "body": {
+                    "id": "chatcmpl-11111",
+                    "object": "chat.completion",
+                    "created": 0,
+                    "model": "gpt-4o",
+                    "choices": [
+                        {
+                            "index": 0,
+                            "message": {
+                                "role": "assistant",
+                                "content": "Lorem ipsum, dolor sit amet.",
+                            },
+                            "finish_reason": "stop",
+                        }
+                    ],
+                    "usage": {
+                        "prompt_tokens": 24,
+                        "completion_tokens": 15,
+                        "total_tokens": 39,
+                    },
+                    "system_fingerprint": None,
+                },
+            },
+            "error": None,
+        }
+        for i in range(n)
+    ]
+
+    # Encode as bytes
+    response_body_bytes = "\n".join(json.dumps(res) for res in response_body).encode(
+        "utf-8"
+    )
+
+    return response_body_bytes
+
+
+def retrieve_mock_gen(timeout: int = 10):
+    """
+    Generates a 'stateful' function that simulates
+    the API processing the batch request
+    """
+    t_start = time.time()
+    inner_batch = copy(mock_batch)
+
+    async def inner(self, batch_id, **kwargs):
+        if time.time() - t_start < timeout:
+            inner_batch.status = "in_progress"
+            return inner_batch
+        else:
+            inner_batch.status = "completed"
+            return inner_batch
+
+    return inner
+
+
+class DummyAgent(agents.Agent):
+    SYSTEM_PROMPT = "You're an agent."
+    BASE_PROMPT = "Do something.\n{batch}"
+
+    def __init__(self, provider=None, **kwargs):
+        super().__init__(
+            stopping_condition=agents.StopOnStep(1), provider=provider, **kwargs
+        )
+
+
+def mock_oai(mocker: MockFixture, n_queries: int = 1, batch_timeout: int = 10):
+    mock_response = mocker.Mock(spec=HttpxBinaryResponseContent)
+    # Generate a mock response body
+    response_body_bytes = gen_response_body(n_queries)
+    mocker.patch.object(mock_response, "content", response_body_bytes)
+    # Patch AsyncAzureOpenAI:
+    # - skip __init__
+    # - Patch create method of files class
+    # - Patch create and retrieve methods of batches
+    mocker.patch.object(openai.AsyncAzureOpenAI, "__init__", return_value=None)
+    mocker.patch.object(AsyncFiles, "create", return_value=mock_file_obj)
+    mocker.patch.object(AsyncFiles, "content", return_value=mock_response)
+    mocker.patch.object(AsyncBatches, "create", return_value=mock_batch)
+    mocker.patch.object(AsyncBatches, "retrieve", retrieve_mock_gen(batch_timeout))
+
+
+@pytest.mark.asyncio
+async def test_batch_mocking(mocker: MockFixture):
+    """
+    Test that our mocking of OpenAI still works
+    since it's jank and changes to the upstream API might break it.
+    """
+    mock_oai(mocker)
+    aoi = openai.AsyncAzureOpenAI()
+    aoi.files = AsyncFiles(aoi)
+    aoi.batches = AsyncBatches(aoi)
+
+    # Test file and batch creation endpoint actually return
+    # expected objects
+    assert (
+        await aoi.files.create(file=mock_batch_file_ret, purpose="batch")
+        == mock_file_obj
+    )
+    assert (
+        await aoi.batches.create(
+            completion_window="24h",
+            endpoint="/v1/chat/completions",
+            input_file_id="file_123",
+        )
+        == mock_batch
+    )
+
+    # Test that our simulation of the batch API works
+    # - Should return in_progress followed by completed after a timeout
+    outer_batch = copy(mock_batch)
+    outer_batch.status = "in_progress"
+    assert await aoi.batches.retrieve("batch_123") == outer_batch
+    outer_batch.status = "completed"
+    await asyncio.sleep(10)
+    assert await aoi.batches.retrieve("batch_123") == outer_batch
+
+    # Test that batch content comes through ok
+    file_content = await aoi.files.content("file_123")
+    assert file_content.content == gen_response_body(1)
+
+
+@pytest.mark.asyncio
+async def test_batch_api(mocker: MockFixture):
+    # Patch AsyncAzureOpenAI
+
+    mock_oai(mocker)
+
+    aoi = openai.AsyncAzureOpenAI()
+    aoi.files = AsyncFiles(aoi)
+    aoi.batches = AsyncBatches(aoi)
+
+    mocker.patch.object(agents.AzureOpenAIBatchProvider, "authenticate", return_value=None)
+    provider = agents.AzureOpenAIBatchProvider("random_model", quiet=True)
+
+    # Patch in mocked OAI API class
+    provider.llm = aoi
+
+    # Set an artificially short timeout for testing
+    provider.batch_handler.api_timeout = 3
+
+    async with provider:
+        ag = DummyAgent(provider=provider, batch="blah")
+        await ag()
+
+    assert ag.answer == "Lorem ipsum, dolor sit amet."
