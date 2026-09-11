b234bef5f440726f9cc5f7cb224bacbb90bb97b9
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2026-09-11 09:30:36 -0400

    Ruff formatted

--- agents/agent/base.py
+++ agents/agent/base.py
@@ -119,9 +119,13 @@ class Agent(_Agent):
 
         # Evaluate callbacks, if available
         for callback in self.CALLBACKS:
-            await self._handle_callback(callback, self, answer=self.answer, scratchpad=self.scratchpad)
+            await self._handle_callback(
+                callback, self, answer=self.answer, scratchpad=self.scratchpad
+            )
 
-    async def _handle_callback(self, func: Callable[..., Awaitable[None]], *args, **kwargs):
+    async def _handle_callback(
+        self, func: Callable[..., Awaitable[None]], *args, **kwargs
+    ):
         """
         Wrapper to handle callback gracefully
         """
